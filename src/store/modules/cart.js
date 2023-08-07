d8caf067d42c712ccc06a08d9fbac6894ddfa47b
c.bisagni <c.bisagni@accenture.com>
2023-08-07 22:04:24 +0200

    improving code

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,68 +1,60 @@
-const state = {
-  cartItems: [],
-};
+export const cart = {
+  state: {
+    cartItems: [],
+  },
+  getters: {
+    cartItemCount: (state) => {
+      return state.cartItems.reduce((total, item) => total + item.cantidad, 0);
+    },
+    getCartItemById: (state) => (productId) => {
+      return state.cartItems.find((item) => item.id === productId);
+    },
+    cartItems: state => state.cartItems,
+    calculateTotalPrice: (state) => {
+      return state.cartItems.reduce((total, item) => total + item.precio * item.cantidad, 0);
+    },
+  },
+  mutations: {
+    addToCart(state, product) {
+      state.cartItems.push(product)
+    },
+    updateCartItemQuantity(state, { productId, quantity }) {
+      const existingItem = state.cartItems.find((item) => item.id === productId);
 
-const getters = {
-  cartItemCount: (state) => {
-    return state.cartItems.reduce((total, item) => total + item.cantidad, 0);
+      if (existingItem) {
+        existingItem.cantidad = quantity;
+      }
+    },
+    removeProductFromCart(state, productId) {
+      state.cartItems = state.cartItems.filter((item) => item.id !== productId);
+    },
+    clearCartItems(state) {
+      state.cartItems = [];
+    },
   },
-  getCartItemById: (state) => (productId) => {
-    return state.cartItems.find((item) => item.id === productId);
-  },
-  cartItems: state => state.cartItems,
-  calculateTotalPrice: (state) => {
-    return state.cartItems.reduce((total, item) => total + item.precio * item.cantidad, 0);
-  },
-};
+  actions: {
+    addProductToCart({ commit, getters }, product) {
+      const cartItem = getters.getCartItemById(product.id);
 
-const mutations = {
-  addToCart(state, product) {
-    state.cartItems.push(product)
-  },
-  updateCartItemQuantity(state, { productId, quantity }) {
-    const existingItem = state.cartItems.find((item) => item.id === productId);
-
-    if (existingItem) {
-      existingItem.cantidad = quantity;
-    }
-  },
-  removeProductFromCart(state, productId) {
-    state.cartItems = state.cartItems.filter((item) => item.id !== productId);
-  },
-  clearCartItems(state) {
-    state.cartItems = [];
-  },
-};
-
-const actions = {
-  addProductToCart({ commit, getters }, product) {
-    const cartItem = getters.getCartItemById(product.id);
-
-    if (cartItem) {
-      commit("updateCartItemQuantity", { productId: product.id, quantity: cartItem.cantidad + 1 });
-    } else {
-      commit("addToCart", { ...product, cantidad: 1 });
-    }
-  },
-  incrementQuantity({ commit }, product) {
-    commit("updateCartItemQuantity", { productId: product.id, quantity: product.cantidad + 1 });
-  },
-  decrementQuantity({ commit }, product) {
-    if (product.cantidad > 0) {
-      commit("updateCartItemQuantity", { productId: product.id, quantity: product.cantidad - 1 });
-    }
-  },
-  removeProductFromCart({ commit }, productId) {
-    commit("removeProductFromCart", productId);
-  },
-  vaciarCarrito({ commit }) {
-    commit("clearCartItems");
-  },
-};
-
-export default {
-  state,
-  getters,
-  mutations,
-  actions,
-};
+      if (cartItem) {
+        commit("updateCartItemQuantity", { productId: product.id, quantity: cartItem.cantidad + 1 });
+      } else {
+        commit("addToCart", { ...product, cantidad: 1 });
+      }
+    },
+    incrementQuantity({ commit }, product) {
+      commit("updateCartItemQuantity", { productId: product.id, quantity: product.cantidad + 1 });
+    },
+    decrementQuantity({ commit }, product) {
+      if (product.cantidad > 0) {
+        commit("updateCartItemQuantity", { productId: product.id, quantity: product.cantidad - 1 });
+      }
+    },
+    removeProductFromCart({ commit }, productId) {
+      commit("removeProductFromCart", productId);
+    },
+    vaciarCarrito({ commit }) {
+      commit("clearCartItems");
+    },
+  }
+}
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,40 +1,32 @@
-const state = {
-  user: null
-};
-
-const getters = {
-  user: state => state.user,
-  loggedIn: state => !!state.user,
-  isAdmin: state => state.user?.isAdmin || false
-};
-
-const mutations = {
-  setUser(state, user) {
-    state.user = user;
+export const cart = {
+  state: {
+    user: null
   },
-  clearUser(state) {
-    state.user = null;
+  getters: {
+    user: state => state.user,
+    loggedIn: state => !!state.user,
+    isAdmin: state => state.user?.isAdmin || false
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    clearUser(state) {
+      state.user = null;
+    }
+  },
+  actions: {
+    loginUser({ commit }, user) {
+      localStorage.setItem("user", JSON.stringify(user));
+      commit("setUser", user);
+    },
+    logoutUser({ commit }) {
+      localStorage.removeItem("user");
+      commit("clearUser");
+    },
+    initializeUser({ commit }) {
+      const user = JSON.parse(localStorage.getItem("user"));
+      commit("setUser", user);
+    }
   }
-};
-
-const actions = {
-  loginUser({ commit }, user) {
-    localStorage.setItem("user", JSON.stringify(user));
-    commit("setUser", user);
-  },
-  logoutUser({ commit }) {
-    localStorage.removeItem("user");
-    commit("clearUser");
-  },
-  initializeUser({ commit }) {
-    const user = JSON.parse(localStorage.getItem("user"));
-    commit("setUser", user);
-  }
-};
-
-export default {
-  state,
-  getters,
-  mutations,
-  actions
-};
+}
